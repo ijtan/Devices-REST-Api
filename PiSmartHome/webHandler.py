562f0691bf655e4e9a97e189722e1b739435f772
ijtan <ethanzammit@gmail.com>
2020-11-19 21:42:11 +0100

    removed old implementation

--- PiSmartHome/webHandler.py
+++ PiSmartHome/webHandler.py
@@ -1,88 +1,118 @@
-import flask
-from flask import request, jsonify,Flask,make_response, abort
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask.wrappers import Response
+from flask_restful import Resource, Api, abort, reqparse
 import json
-
-
-
-def saveDeviceList():
-    json.dump(deviceList, open(filename, 'w'))
-
-def loadDeviceList():
-    global deviceList
-    deviceList = json.load(open(filename, 'r'))
-
-filename = "config\devices.json"
-deviceList = ""
-loadDeviceList()
-
+import os
 
 app = Flask(__name__)
 api = Api(app)
-app.config["DEBUG"] = True
 
-#API HANDLING
+class device(Resource):
+        
+    def get(self, device_id):
+        return {device_id: getDevice(device_id)}        
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    def put(self, device_id):
+        parser.add_argument('name', required=True, help="Name cannot be blank!")
+        parser.add_argument('mac', required=True, help="Mac Address cannot be blank!")
 
-@app.route('/api/led/', methods=['GET'])
-def led_all():
-    json.load("config/ledStripState.json")
-    return jsonify()
+        args = parser.parse_args()
+        device = getDevice(device_id)
+        args.found = False
+        args.date = args.ip = ""
+        updateDeviceByArgs(device,args)
+        return jsonify(getDevice(device_id))
+
+    # def patch(self, device_id):
+    #     parser.add_argument('name')
+    #     parser.add_argument('mac')
+    #     args = parser.parse_args()
+    #     if(all(arg is None for arg in args.values())):
+    #         abort(400,message="No args found")
+    #     device = getDevice(device_id)
+    #     updateDeviceByArgs(device, args)
+
+    #     return {device_id: getDevice(device_id)}
+    
+    def delete(self,device_id):
+        deleteDevice(device_id)
+        return '', 204
+    
+
+api.add_resource(device, '/api/devices/<int:device_id>')
 
 
+class allDevices(Resource):
+    def post(self):
+        parser.add_argument('name', required=True, help="Name cannot be blank!")
+        parser.add_argument('mac', required=True, help="Mac Address cannot be blank!")
 
-@app.route('/api/devices/', methods=['GET'])
-def devices_all():
-    return jsonify(deviceList)
+        args = parser.parse_args()
+        if(not devices):
+            id = 0;
+        else:
+            id = max([num.get('id') for num in devices]) + 1
+        args.id = id
+        args.found = False
+        args.date = args.ip = ""
 
-@app.route('/api/devices/<int:device_id>', methods=['GET'])
-def get_device(device_id):
-    device = [device for device in deviceList if device.get('id') == device_id]
-    if len(device) == 0:
-        abort(404)
-    return jsonify({'device': device[0]})
+        addDevice(args)
+        return jsonify(getDevice(id))
 
-@app.route('/api/devices/', methods=['POST'])
-def add_device():
-    if not request.json or not 'name' in request.json or not 'mac' in request.json:
-        print('error',request.form['data'])
-        abort(400)
-    id = max([num.get('id') for num in deviceList]) + 1
-    name = request.json.get('title')
-    mac = request.json.get('mac')
-    device = {'name': name,'mac':mac,'id':id}
-    deviceList.append(device)
+    def get(self):
+        loadDeviceList()
+        return jsonify(devices)
+
+api.add_resource(allDevices, '/api/devices/')
+def saveDeviceList():
+        json.dump(devices,open(filename, 'w'), ensure_ascii=False, indent=4)
+
+def loadDeviceList():
+    global devices
+
+    f = open(filename, 'a')
+    f.close()
+    filesize = os.path.getsize(filename)
+    print('fs',filesize)
+    if (not filesize):
+        return
+
+    devices = json.load(open(filename, 'r'))
+    id = 0;
+    for d in devices:
+        if not d.get('id'):
+            d.setdefault('id', id)
+            id+=1
+        else:
+            id=d.get('id')+1
+
+def addDevice(device):
+    devices.append(device)
     saveDeviceList()
-    return jsonify({'device': device}), 201
+    
+def getDevice(id):
+    for device in devices:
+        if device.get('id') == id:
+            return device
+    abort(404,message="Device not found")
 
-@app.route('/api/devices/<int:device_id>', methods=['PUT'])
-def update_task(device_id):
-    device = [device for device in deviceList if device['id'] == device_id]
-    if len(device) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    # if 'address' in request.json and type(request.json['address']) != UNICODE:
-    #     abort(400)
-    # if 'description' in request.json and type(request.json['description']) is not unicode:
-    #     abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    device[0]['title'] = request.json.get('title', device[0]['title'])
-    device[0]['description'] = request.json.get('description', device[0]['description'])
-    device[0]['done'] = request.json.get('done', device[0]['done'])
-    return jsonify({'device': device[0]})
-
-@app.route('/api/devices/<int:device_id>', methods=['DELETE'])
-def delete_task(device_id):
-    device = [device for device in deviceList if device['id'] == device_id]
-    if len(device) == 0:
-        abort(404)
-    device.remove(device[0])
+def updateDeviceByArgs(device, args):
+    for key,value in args.items():
+            if value:
+                print("setting",key,"to",value)
+                device[key] = value
     saveDeviceList()
-    return jsonify({'result': True})
 
-Flask.run(app)
+def deleteDevice(id):
+    devices.remove(getDevice(id))
+    saveDeviceList()
+
+
+
+
+if __name__ == '__main__':
+    parser = reqparse.RequestParser()
+    filename = "config\devices.json"
+    devices = []
+    loadDeviceList()
+    app.run(debug=True)
